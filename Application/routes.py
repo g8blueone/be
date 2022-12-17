75af554232199e8b8031f406a815c6f6d9fceb11
VladCeontea <ceonteavladcalin@yahoo.com>
2022-12-17 20:39:46 +0200

    added patients table + basic routes

--- Application/routes.py
+++ Application/routes.py
@@ -2,6 +2,7 @@ import datetime
 
 from flask import session, request
 
+from Application.Model.Patients import Patients
 from Application.app import create_app, database
 from datetime import timedelta
 from Application.Model.Appointments import Appointments
@@ -61,6 +62,31 @@ def index2(id):
         appointment.date = datetime.datetime.strptime(new_appointment['date'], '%Y-%m-%d').date()
         appointment.time = datetime.datetime.strptime(new_appointment['time'], '%H:%M').time()
         appointment.type = new_appointment["type"]
+        database.session.commit()
+        return jsonify(request.json), 200
+
+
+@app.route('/patient/<cnp>', methods=["GET", "PUT"])
+def patient_home(cnp):
+    if request.method == "GET":
+        patient = Patients.query.filter_by(cnp_patient=cnp).first()
+
+        return jsonify(patient.serialize())
+
+    elif request.method == "PUT":
+        new_patient = request.json
+
+        patient = Patients.query.filter_by(cnp_patient=cnp).first()
+        patient.email = new_patient["email"]
+        patient.password = new_patient["password"]
+        patient.first_name = new_patient["first_name"]
+        patient.last_name = new_patient["last_name"]
+        patient.address = new_patient["address"]
+        patient.city = new_patient["city"]
+        patient.county = new_patient["county"]
+        patient.country = new_patient["country"]
+        patient.date_of_birth = datetime.datetime.strptime(new_patient['date_of_birth'], '%Y-%m-%d').date()
+
         database.session.commit()
         return jsonify(request.json), 200
 
