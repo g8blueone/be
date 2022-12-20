5d21c4b64bcd6db60b8a65fa3adb6a8214d1c4d3
TC <tudor.campean23@gmail.com>
2022-12-20 22:04:36 +0200

    added doctors

---
+++ Application/Utils/doctors_query_utils.py
@@ -0,0 +1,17 @@
+import math
+
+from Application.Utils import constants
+
+
+def paginate(doctors, query):
+    try:
+        page_position = query.get("page")
+        if page_position:
+            page = int(page_position)
+            return doctors[(page - 1) * constants.pagesize: (page - 1) * constants.pagesize + constants.pagesize]
+    except:
+        return doctors
+    return doctors
+
+def get_total_of_pages(doctors):
+    return math.ceil(doctors.count() / constants.pagesize)
