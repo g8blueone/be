da22207a7ddbcf29bd0dc7268505780db763f0bb
VladCeontea <ceonteavladcalin@yahoo.com>
2023-01-21 15:26:50 +0200

    Update appointment_routes.py

--- Application/appointment_routes.py
+++ Application/appointment_routes.py
@@ -39,7 +39,7 @@ def index():
                                    datetime.datetime.strptime(new_appointment['time'], '%H:%M').time(), new_appointment["type"])
 
         if new_appointment["user_type"] == "patient":
-            busy_doctors = database.session.query(Appointments.doctor_id).filter(and_(Appointments.date == new_appointment['date'], Appointments.time == new_appointment['time'])).distinct().all()
+            busy_doctors = Appointments.query.with_entities(Appointments.doctor_id).filter(and_(Appointments.date == new_appointment['date'], Appointments.time == new_appointment['time'])).distinct().all()
             busy_doctors_list = [doc for (doc,) in busy_doctors]
             doctor_id = Doctors.query.filter(and_(Doctors.specialization == new_appointment["type"], Doctors.id_doctor not in busy_doctors_list)).first().get_id()
             appointment = Appointments(new_appointment["token"], doctor_id, new_appointment["location"], datetime.datetime.strptime(new_appointment['date'], '%Y-%m-%d').date(),
